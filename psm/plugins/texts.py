add53f20fff10373543fce79560d212724231306
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2020-12-03 21:10:32 +0100

    Update texts.py

--- psm/plugins/texts.py
+++ psm/plugins/texts.py
@@ -7,25 +7,25 @@ By @DamienSoukara
 """
 
 helptext1 = """
-**📖 Help (1 of 3)**
+[📖](https://i.imgur.com/6jqpx7g.jpg) **Help (1 of 3)**
 you can get API_ID and API_HASH value by visiting https://my.telegram.org or @DamienTgOrgBot
 """
 
 helptext2 = """
-**📖 Help (2 of 3)**
+[📖](https://i.imgur.com/hXgV9Z1.jpg) **Help (2 of 3)**
 Telegram will send validation to you,
 
 **MAKE SURE** to type validation code in this format: `1-2-3-4-5` and not `12345`
 """
 helptext3 = """
-**📖 Help (3 of 3)**
+[📖](https://i.imgur.com/NK37JBt.jpg) **Help (3 of 3)**
 
 When you are ready you can start with /proceed
 You can also cancel current operation with /start
 """
 
 tiptext1 = """
-**💡 Extra Help:**
+[💡](https://i.imgur.com/hxv8w4M.png) **Extra Help:**
 
 If you don't understand properly and need more help than ask in [Support Group](t.me/damienhelp)
 
